4d8699011178ee2c54770413a6e46f299e49b8ab
hassan <hassanmujtaba8911@gmail.com>
2026-04-03 20:15:09 +0500

    Merge Sort and Floyd's Algorithm

--- SinglyLinkedList/Algorithms.c
+++ SinglyLinkedList/Algorithms.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<stdbool.h>
 typedef struct Node{
 	int data;
 	struct Node* next;	
@@ -32,6 +32,7 @@ void printList(Node* head){
 	}
 	printf("NULL\n");
 }
+
 //Reversing List
 //Time Complexity:O(n) bcz in worst case , it will run n time
 //No auxiliary space increasing so space complexity is constant O(1)
@@ -56,7 +57,6 @@ Node* reverseList(Node**head,Node**tail){
 	*head=prev;	
 	return *head;	//when head will NULL and loop ends, previous pointer will be at last node so its new head
 }
-
 
 //Recursive Reverse List Function
 //Time Complexity: Linear O(n)
@@ -92,6 +92,104 @@ Node* mergeList(Node* head1, Node* head2) {
     	return head2;
     }
 }
+
+
+//problem: Os, 1s, 2s
+//Given list containing 0, 1 and 2
+//Sort the List
+//Solution: Counting every 0, 1 , and 2 and then changing values
+Node* threeProblem(Node* head){	
+	Node* temp=head;
+	int zeroCounter=0;
+	int oneCounter=0;
+	int twoCounter=0;
+	
+	while(temp!=NULL){
+		if(temp->data==0){
+			zeroCounter++;
+		}
+		else if(temp->data==1){
+			oneCounter++;
+		}
+		else{
+			twoCounter++;
+		}
+		temp=temp->next;	//moving temp to next
+	}
+	temp=head;
+	while(temp!=NULL){		//while not reaching end, swaping values
+		if(zeroCounter>0){	//first writing all zeros
+			temp->data=0;
+			zeroCounter--;
+		}
+		else if(oneCounter>0){	//if zero finished, then 1
+			temp->data=1;	
+			oneCounter--;
+		}
+		else{					//remaining all are 2 so insert them
+			temp->data=2;
+			twoCounter--; 		//reduant 
+		}
+		temp=temp->next;
+	}
+	return head;
+	//Time Complexity: two loops mean O(n) + O(n) =O(2n) 
+	//Constants are ignored so O(n)
+	//Space complexity = O(1) bcz no memory 
+}
+
+//Helper function to find mid
+Node* findMid(Node* head){
+	Node* slow=head;
+	Node* fast=head;
+	//in case of even sized lsit, considering second as mid
+	while(fast!=NULL && fast->next!=NULL){ //fast->next case for even list
+		slow=slow->next;
+		fast=fast->next->next;
+	}
+	return slow;
+}
+
+//Recursive Approach Using Merge Sort
+//Time Complexity O(n log n)
+//Space Complexity O(log n) bcz roughly half size with every call
+Node* threeProblemRec(Node* head){
+	if(head==NULL || head->next==NULL){
+		return head;
+	}
+	//Using two helper function: 1 for finding mid and 2nd for merging two halves into one
+	Node* mid=findMid(head);
+	Node* secondHead=mid->next;		//holding remaining half of list
+	mid->next=NULL;
+	/* Before applying merge sort, need to sort the lists first */
+	Node* firstHalve=threeProblemRec(head);
+	Node* secondHalve=threeProblemRec(secondHead);
+	
+	return mergeList(firstHalve,secondHalve);
+}
+
+
+//Floyd's Cycle Detection Algorithm + Removal
+//Time Complexity: O(n) bcz n operation in worst case (n mean size of input)
+//Space Complexity is constant
+/*
+When both starting moving, distance between them increase by one ,
+a point comes when the distance between them is equal to size of the 
+loop and both meet at a point then its mean cycle exists 
+*/
+bool cycleExists(Node* head){
+	Node* slow=head;
+	Node* fast=head;
+	while(fast!=NULL && fast->next!=NULL){
+		slow=slow->next;
+		fast=fast->next->next;
+		if(slow==fast){
+			return true;
+		}
+	}
+	return false;
+}
+
 
 int main(){
 	Node* head=NULL;
